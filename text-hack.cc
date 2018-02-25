f3f8acda1fc0cdf9cbf59f7bb0d8cd4cc7498abe
nanoscopic <github@codechild.com>
2018-02-25 12:04:17 -0800

    Use regular char type instead of unsigned char

--- text-hack.cc
+++ text-hack.cc
@@ -28,9 +28,8 @@ int main( int argc, char *argv[] ) {
     FILE *data_handle = 0;
     bool showhelp = 0;
     if( argc < 2 ) {
-        if( isatty( STDIN_FILENO ) ) {
+        if( isatty( STDIN_FILENO ) )
             showhelp = 1;
-        }
         else {
             fprintf(stderr, "Reading input from redirection\n" );
             data_handle = stdin;
@@ -62,20 +61,18 @@ int main( int argc, char *argv[] ) {
         return 0;
     }
         
-    if( !data_handle ) {
-        return 1;
-    }
+    if( !data_handle ) return 1;
     
     #define STACK_SIZE MIN_SEQUENCE + 1
-    unsigned char stack[ STACK_SIZE ];
+    char stack[ STACK_SIZE ];
     memset( stack, 0, STACK_SIZE );
     
     // Build accept charmap
-    unsigned char accept[ 256 ];
+    char accept[ 256 ];
     memset( accept, 0, 256 );
-    unsigned char accept_these[] = OK_CHARS;
+    char accept_these[] = OK_CHARS;
     for( int i=0; i < sizeof( accept_these ); i++ ) {
-        unsigned char let = accept_these[ i ];
+        char let = accept_these[ i ];
         accept[ let ] = 1;
     }
     
@@ -84,27 +81,23 @@ int main( int argc, char *argv[] ) {
     int stack_fill = 0; // how much of the stack is filled
     bool stack_full = 0;
     while( 1 ) {
-        unsigned char let = fgetc( data_handle );
+        char let = fgetc( data_handle );
         if( feof( data_handle ) ) break;
         
         if( !stack_full ) {
-            stack_fill++;
-            if( stack_fill == MIN_SEQUENCE ) {
-                stack_full = 1;
-            }
+            if( ++stack_fill == MIN_SEQUENCE ) stack_full = 1;
         }
         
         // shift out the lowest char
-        unsigned char n_ago = stack[ 0 ];
+        char n_ago = stack[ 0 ];
         
-        for( int i=0;i<(MIN_SEQUENCE-1);i++ ) {
+        for( int i=0;i<(MIN_SEQUENCE-1);i++ ) 
             stack[ i ] = stack[ i + 1 ];
-        }
         
         // push the new character onto the stack and pop the last one
         stack[ MIN_SEQUENCE - 1 ] = let;
         
-        unsigned char num_ok = 0;
+        char num_ok = 0;
         if( accept[ n_ago ] ) {
             num_ok++;
             for( int i=0;i<=(MIN_SEQUENCE-1);i++ ) {
@@ -112,9 +105,8 @@ int main( int argc, char *argv[] ) {
                 else i=100;
             }
         }
-        else {
+        else
             ok_cnt = 0;
-        }
         
         if( ( num_ok + ok_cnt ) >= MIN_SEQUENCE ) {
             putchar( n_ago );
@@ -125,8 +117,8 @@ int main( int argc, char *argv[] ) {
             ok_cnt = 0;
         }
     }
-    for( int i=0;i<stack_fill;i++ ) {
+    for( int i=0;i<stack_fill;i++ )
         putchar( stack[ i ] );
-    }
+
     fclose( data_handle );
 }
