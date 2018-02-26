d02ae89bd09bbef15cfd630dea7c7cd0344adf20
nanoscopic <github@codechild.com>
2018-02-25 18:17:03 -0800

    Use faux objects Re-structured code to work using faux objects; better for proper cleanup and file closing as well as avoiding globals.

--- text-hack.c
+++ text-hack.c
@@ -29,52 +29,120 @@
 
 #define bool char
 
-FILE *data_handle;
-uint32_t get_file_blocksize( FILE *fh ) {
-    int fno = fileno( data_handle );
+typedef struct {
+    FILE *data_handle;
+    uint32_t blocksize;
+    char *buffer;
+    bool needclose;
+    uint32_t bufferpos;
+    uint32_t buffered;
+} data_c;
+
+data_c *data_c__new() {
+    data_c *self = malloc( sizeof( data_c ) );
+    memset( self, 0, sizeof( data_c ) );
+    return self;
+}
+
+void data_c__delete( data_c *self ) {
+    if( self->needclose ) {
+        fclose( self->data_handle );
+    }
+    if( self->buffer ) {
+        free( self->buffer );
+    }
+    free( self );
+}
+
+void data_c__read_blocksize( data_c *self ) {
+    int fno = fileno( self->data_handle );
     struct stat fi;
     fstat(fno, &fi);
     if( !fi.st_blksize ) {
         fprintf(stderr, "File blocksize unclear; defaulting to 512\n" );
     }
-    return fi.st_blksize || 512;
+    self->blocksize = fi.st_blksize || 512;
 }
 
-uint32_t blocksize;
-char *buffer;
-
-uint32_t bufferpos = 0;
-uint32_t buffered = 0;
-bool get_byte( uint8_t *byte ) {
-    if( !buffered ) {
-        size_t bytes_read = fread( buffer, blocksize, 1, data_handle );
-        if( !bytes_read ) return 0;
-        buffered = bytes_read;
-        bufferpos = 0;
+bool data_c__open_file( data_c *self, char *filename ) {
+    self->data_handle = fopen( filename, "r" );
+    if( !self->data_handle ) {
+        fprintf(stderr, "File does not exist\n" );
+        return 1;
     }
-    *byte = buffer[ bufferpos++ ];
-    buffered--;
+    self->needclose = 1;
+    data_c__read_blocksize( self );
+    self->buffer = malloc( self->blocksize );
+    return 0;
+}
+
+void data_c__use_fh( data_c *self, FILE *fh ) {
+    self->data_handle = fh;
+    data_c__read_blocksize( self );
+    self->buffer = malloc( self->blocksize );
+}
+
+bool data_c__get_byte( data_c *self, uint8_t *byte ) {
+    if( !self->buffered ) {
+        size_t bytes_read = fread( self->buffer, self->blocksize, 1, self->data_handle );
+        if( !bytes_read ) return 0;
+        self->buffered = bytes_read;
+        self->bufferpos = 0;
+    }
+    *byte = self->buffer[ self->bufferpos++ ];
+    self->buffered--;
     return 1;
 }
 
+typedef struct {
+    bool accept[ 256 ];
+} byte_match_c;
+
+byte_match_c *byte_match_c__new() {
+    byte_match_c *self = malloc( sizeof( byte_match_c ) );
+    memset( self, 0, sizeof( byte_match_c ) );
+    return self;
+}
+
+void byte_match_c__delete( byte_match_c *self ) {
+    free( self );
+}
+
+void byte_match_c__accept_chars_w_len( byte_match_c *self, char *accept_these, int len ) {
+    //char accept_these[] = OK_CHARS;
+    for( unsigned int i=0; i < len; i++ ) {
+        char let = accept_these[ i ];
+        self->accept[ let ] = 1;
+    }
+}
+
+bool byte_match_c__accept( byte_match_c *self, uint8_t byte ) {
+    return self->accept[ byte ];
+}
+
+bool data_c__source_valid( data_c *self ) {
+    return self->data_handle ? 1 : 0;
+}
+
 int main( int argc, char *argv[] ) {
-    data_handle = 0;
+    data_c *data_ob = data_c__new();
+    
     bool showhelp = 0;
     if( argc < 2 ) {
         if( isatty( STDIN_FILENO ) )
             showhelp = 1;
         else {
             fprintf(stderr, "Reading input from redirection\n" );
-            data_handle = stdin;
+            data_c__use_fh( data_ob, stdin );
         }
     }
     
     if( argc > 1 ) {
         if( !strncmp( argv[1], "--help", 6 ) ) showhelp = 1;
         fprintf(stderr, "Using file '%s' for input\n", argv[1] );
-        data_handle = fopen( argv[1], "r" );
-        if( !data_handle ) {
-            fprintf(stderr, "File does not exist\n" );
+        bool err = data_c__open_file( data_ob, argv[1] );
+        if( err ) {
+            data_c__delete( data_ob );
             return 1;
         }
     }
@@ -91,34 +159,29 @@ int main( int argc, char *argv[] ) {
         
         printf("Usage:\n  %s [filename]\n  %s < filename\n", base, base );
         
+        data_c__delete( data_ob );
         return 0;
     }
         
-    if( !data_handle ) return 1;
+    if( !data_c__source_valid( data_ob ) ) {
+        data_c__delete( data_ob );
+        return 1;
+    }
     
-    blocksize = get_file_blocksize( data_handle );
-    buffer = malloc( blocksize );
-        
     #define STACK_SIZE MIN_SEQUENCE + 1
     char stack[ STACK_SIZE ];
     memset( stack, 0, STACK_SIZE );
     
-    // Build accept charmap
-    char accept[ 256 ];
-    memset( accept, 0, 256 );
-    char accept_these[] = OK_CHARS;
-    for( unsigned int i=0; i < ( sizeof( accept_these ) - 1 ); i++ ) {
-        char let = accept_these[ i ];
-        accept[ let ] = 1;
-    }
-    
+    byte_match_c *byte_match_ob = byte_match_c__new();
+    byte_match_c__accept_chars_w_len( byte_match_ob, OK_CHARS, sizeof( OK_CHARS ) - 1 );
+        
     uint8_t prev_ok = 0;
     
     int stack_fill = 0; // how much of the stack is filled
     bool stack_full = 0;
     uint8_t byte;
     while( 1 ) {
-        if( !get_byte( &byte ) ) break;
+        if( !data_c__get_byte( data_ob, &byte ) ) break;
         
         if( !stack_full ) {
             if( ++stack_fill == MIN_SEQUENCE ) stack_full = 1;
@@ -135,10 +198,10 @@ int main( int argc, char *argv[] ) {
         stack[ MIN_SEQUENCE - 1 ] = byte;
         
         char ok_in_stack = 0;
-        if( accept[ n_ago ] ) {
+        if( byte_match_c__accept( byte_match_ob, n_ago ) ) {
             ok_in_stack++;
             for( int i=1;i<MIN_SEQUENCE;i++ ) {
-                if( accept[ stack[ i ] ] ) ok_in_stack++;
+                if( byte_match_c__accept( byte_match_ob, stack[ i ] ) ) ok_in_stack++;
                 else break;
             }
         }
@@ -157,6 +220,8 @@ int main( int argc, char *argv[] ) {
     for( int i=0;i<stack_fill;i++ )
         putchar( stack[ i ] );
 
-    fclose( data_handle );
-    if( buffer ) free( buffer );
+    byte_match_c__delete( byte_match_ob );
+    data_c__delete( data_ob );
+    
+    return 0;
 }
